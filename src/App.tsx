003a1f50983a7d25d2b75f294063b8047ca3f228
yakshao <akshyapatill@gmail.com>
2024-08-20 06:07:18 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink, useLocation} from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export function App() {
 
   const [showFixedNav, setShowFixedNav] = useState(false);
+
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll)
 
   })
 
+
   function handleScroll() {
     window.scrollY > 70 ? setShowFixedNav(true) : setShowFixedNav(false);
 
   }
+
+
 
 
   return (
@@ -59,11 +65,17 @@ function BarC() {
 }
 
 function Header() {
+  const {pathname} = useLocation();
+  useEffect(()=>{
+    window.scrollTo(0, 0)
+  }, [pathname])
 
   return (<div className='pHeader'>
     <div className="pHeaderLogo">
-      <img
-        src='/logo.png' />
+     <NavLink to='/'>
+        <img
+          src='/logo.png' />
+     </NavLink>
     </div>
     <div className='pHeaderNav'>
       <div className='navLinks flex'>
@@ -89,17 +101,26 @@ function Header() {
 function StickyNav({ showFixedNav }: any) {
 
   return <div className={showFixedNav ? 'stickyNavAnimStart stickyNav' : 'stickyNavAnimEnd stickyNav'}>
-
-    <div className='flex'>
-      <img
-        src='/logo.png' />
-
+ <NavLink to='/'>
+    <div className='flex center'>
+    
+        <img
+          src='/logo.png' />
+    
     </div>
+     </NavLink>
+
     <div className='stickyHeaderNav'>
       <div className='navLinks flex'>
-        <div className='navLink'> <NavLink to={'/'}><a href=''>Home</a></NavLink></div>
-        <div className='navLink'> <NavLink to={'/services'}><a href=''>Services</a></NavLink></div>
-        <div className='navLink'> <NavLink to={'/services'}><a href=''>Portfolio</a></NavLink></div>
+      <div className='navLink'> <NavLink 
+                        className={({ isActive, isPending }) => isActive ? 'current2' : ''}
+        to={'/'}><div><a href=''>Home</a></div></NavLink></div>
+
+        <div className='navLink'> <NavLink 
+                        className={({ isActive, isPending }) => isActive ? 'current2' : ''}
+        to={'/services'}><div><a href=''>Services</a></div></NavLink></div>
+        <div className='navLink'> <NavLink 
+        to={'/services'}><a href=''>Portfolio</a></NavLink></div>
         <div className='navLink'> <NavLink to={'/services'}><a href=''>About Us</a></NavLink></div>
         <div className='navLink'> <NavLink to={'/services'}><a href=''>Contact Us</a></NavLink></div>
         <div className='navLink'> <NavLink to={'/services'}><a href=''>Career</a></NavLink></div>
@@ -239,16 +260,112 @@ function Footer(){
 
   return(
     <div className='footer'>
-      <div data-aos='fade-up'  className='footerChild footerColor1 center'>
-       <h3>Design . Build . Launch</h3>
-       
-       <h1>let's build
-</h1>
-        <h2>
-        something awesome</h2>
+      <div data-aos='fade-up'  className='footerChild footerColor1 center flexColumn'>
+<div>
+         <h3>Design . Build . Launch</h3>
+         
+         <h1>Let's Build
+  </h1>
+          <h3>
+          something awesome</h3>
+</div>
+
+<div className='faIcons'>
+              <div className='faChild'>
+                <FontAwesomeIcon icon={faInstagram} />
+              </div>
+              <div className='faChild'>
+                <FontAwesomeIcon icon={faTwitter} />
+              </div>
+              <div className='faChild'>
+                <FontAwesomeIcon icon={faLinkedin} />
+              </div>
+              <div className='faChild'>
+                <FontAwesomeIcon icon={faFacebook} />
+              </div>
+            </div>
+
       </div>
       <div className='footerChild footerColor2'>
-        <h2>Design.Build.Launch</h2>
+      <div className='footerGrid'>
+      <div>
+        <h6>Overview</h6>
+      </div>
+      <div> 
+     <a href='#'>About Us </a>
+      </div>
+      <div> 
+     <a href='#'>Services</a>
+      </div>
+      <div> 
+     <a href='#'>Portfolio</a>
+      </div>
+      <div> 
+     <a href='#'>Blog</a>
+      </div>
+      <div> 
+     <a href='#'>Contact Us </a>
+      </div>
+      <div> 
+     <a href='#'>Privacy policy </a>
+      </div>
+      <div> 
+     <a href='#'>Terms and Conditions </a>
+      </div>
+      <div> 
+     <a href='#'>Returns and refunds policy </a>
+      </div>
+      <div> 
+     <a href='#'>Shipping policy</a>
+      </div>
+      <div> 
+     <a href='#'>Terms and Conditions </a>
+      </div>
+    </div>
+
+      <div className='footerGrid'>
+      <div><h6>Web solutions</h6></div>
+      <div> 
+     <a href='#'>UI/UX Designs</a>
+      </div>
+      <div> 
+     <a href='#'>Ecommerce Web</a>
+      </div>
+      <div> 
+     <a href='#'>News Portal</a>
+      </div>
+      <div> 
+     <a href='#'>Personal portfolio</a>
+      </div>
+      <div> 
+     <a href='#'>LMS Portal</a>
+      </div>
+</div>
+      
+      <div className='footerGrid'>
+      <div><h6>Digital Marketing</h6></div>
+      <div> 
+     <a href='#'>PPC</a>
+      </div>
+      <div> 
+     <a href='#'>SEO</a>
+      </div>
+      <div> 
+     <a href='#'>CRO</a>
+      </div>
+      <div> 
+     <a href='#'>Logo Design</a>
+      </div>
+      <div> 
+     <a href='#'>Social Media Marketing</a>
+      </div>
+      <div> 
+     <a href='#'>Whatsapp Marketing</a>
+      </div>
+      <div> 
+     <a href='#'>Email Marketing</a>
+      </div>
+      </div>
       </div>
     </div>
    
